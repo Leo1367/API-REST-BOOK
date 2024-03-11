7854633a5d3cc5564b1a010eb463f41634847b6f
Leo1367 <leonardoherculano136@gmail.com>
2024-03-11 19:39:28 -0300

    add verifytoken in bookRoutes

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { controllerBook } from "../controllers/bookControllers.js";
+import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = Router();
 
-router.post('/create-book', controllerBook.createBook)
-router.get('/list-book', controllerBook.listBooks)
-router.get('/:id', controllerBook.getOneBook)
-router.put('/:id', controllerBook.updateBook)
-router.delete('/:id', controllerBook.deleteBook)
+router.post('/create-book', verifyToken, controllerBook.createBook)
+router.get('/list-book', verifyToken, controllerBook.listBooks)
+router.get('/:id', verifyToken, controllerBook.getOneBook)
+router.put('/:id', verifyToken, controllerBook.updateBook)
+router.delete('/:id', verifyToken, controllerBook.deleteBook)
 
 export default router;
